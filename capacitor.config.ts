8983f43d8a5e0146aea5e54dbcd6e2d971a126ca
Amir Javeed <amir.javed@cloudcardinc.com>
2025-08-19 15:45:14 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Employee Management',
+  webDir: 'www'
+};
+
+export default config;
