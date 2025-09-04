dc91b7eb2e1c29cd826531207ea01aadb27b86b0
Amir <amirj9514@gmail.com>
2025-09-04 16:37:02 +0500

    menu bar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonButtons, IonButton, IonFooter } from '@ionic/angular/standalone';
 import { CustomToastComponent } from "./Shared/Components/custom-toast/custom-toast.component";
+import { SvgDirective } from "./Shared/directives/svg.directive";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, CustomToastComponent],
+  imports: [IonApp, IonRouterOutlet, CustomToastComponent, IonMenu, IonHeader, IonToolbar, IonContent, SvgDirective, IonFooter],
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader,IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonContent, IonMenuButton } from '@ionic/angular/standalone';
 import { SvgDirective } from '../Shared/directives/svg.directive';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonContent , SvgDirective],
+  imports: [IonHeader, IonContent, SvgDirective, IonMenuButton],
 })
 export class HomePage {
   constructor() {}
